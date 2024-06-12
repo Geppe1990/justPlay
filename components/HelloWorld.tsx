aa4d36a2e294fd6a2dd699d8a50166c7061bf3a9
Giuseppe Vigneri <geppe1990@gmail.com>
2024-06-12 11:10:00 +0200

    Eslint Prettier

--- components/HelloWorld.tsx
+++ components/HelloWorld.tsx
@@ -1,19 +1,19 @@
 // components/HelloWorld.tsx
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React from "react"
+import { Text, View, StyleSheet } from "react-native"
 
 const HelloWorld = () => {
 	return (
 		<View style={styles.container}>
 			<Text>Hello World!</Text>
 		</View>
-	);
-};
+	)
+}
 
 const styles = StyleSheet.create({
 	container: {
 		padding: 20,
 	},
-});
+})
 
-export default HelloWorld;
+export default HelloWorld
