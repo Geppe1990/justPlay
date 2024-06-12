246279bef52ae8f9a2732de64b554c179fd1a9b8
Giuseppe Vigneri <geppe1990@gmail.com>
2024-06-12 16:57:32 +0200

    Search

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,14 +1,57 @@
 import Ionicons from "@expo/vector-icons/Ionicons"
-import { StyleSheet, Image, Platform } from "react-native"
+import {
+	NativeSyntheticEvent,
+	StyleSheet,
+	TextInput,
+	TextInputChangeEventData,
+	Button,
+	View,
+	Text,
+} from "react-native"
 
-import { Collapsible } from "@/components/Collapsible"
-import { ExternalLink } from "@/components/ExternalLink"
 import ParallaxScrollView from "@/components/ParallaxScrollView"
 import { ThemedText } from "@/components/ThemedText"
 import { ThemedView } from "@/components/ThemedView"
-import Counter from "@/components/Counter"
+import { useState } from "react"
+export interface ResultInterface {
+	name: string
+	id: number
+}
 
+export interface ResultsState {
+	count: number
+	results: ResultInterface[]
+}
 export default function TabThreeScreen() {
+	const [searchGame, setSearchGame] = useState("")
+	const [results, setResults] = useState<ResultsState | null>(null)
+	const handleChangeText = (
+		e: NativeSyntheticEvent<TextInputChangeEventData>
+	): void => {
+		setSearchGame(e.nativeEvent.text)
+	}
+
+	const handlePress = () => {
+		fetchData(searchGame)
+			.then((data) => setResults(data))
+			.catch((error) => console.log(error))
+	}
+
+	const fetchData = async (name: string) => {
+		const response = await fetch(
+			`https://api.rawg.io/api/games?key=${process.env.EXPO_API_KEY}&search=${name}`,
+			{
+				method: "GET",
+				headers: { Accept: "application/json" },
+			}
+		)
+		if (!response.ok) {
+			console.log("ERRORE: ", response)
+		}
+
+		return await response.json()
+	}
+
 	return (
 		<ParallaxScrollView
 			headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
@@ -19,9 +62,21 @@ export default function TabThreeScreen() {
 			<ThemedView style={styles.titleContainer}>
 				<ThemedText type="title">Search</ThemedText>
 			</ThemedView>
-			<ThemedText>
-				This app includes example code to help you get started.
-			</ThemedText>
+			<TextInput
+				style={styles.input}
+				onChange={handleChangeText}
+				value={searchGame}
+				placeholder="Search a game by name"
+				keyboardType="default"
+			/>
+			<Button onPress={handlePress} title="Search" />
+			{results && (
+				<View>
+					{results.results.map((result) => (
+						<ThemedText key={result.name}>{result.name}</ThemedText>
+					))}
+				</View>
+			)}
 		</ParallaxScrollView>
 	)
 }
@@ -36,5 +91,12 @@ const styles = StyleSheet.create({
 	titleContainer: {
 		flexDirection: "row",
 		gap: 8,
+	},
+	input: {
+		height: 40,
+		marginVertical: 12,
+		marginHorizontal: 0,
+		borderWidth: 1,
+		padding: 10,
 	},
 })
